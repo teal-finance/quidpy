991413656c22238185212bd074e0d307e504f236
synw <synw@yahoo.com>
2020-10-24 19:33:31 +0200

    Improve the Flask example server

--- example/server.py
+++ example/server.py
@@ -1,20 +1,42 @@
 from datetime import datetime
-from flask import Flask
+from flask import Flask, abort
+from flask_cors import CORS, cross_origin
 from flask import request
 import jwt
+import os
 
+"""
+To run:
+
+export FLASK_APP=server.py
+flask run
+"""
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 # the key for the "demo" namespace
-key = "d6aa584abd36df336e1b70d777d77eaab5b976c2a119be94a9945cc52e4bffe9"
+key = os.environ["QUID_DEMO_KEY"]
 
 
-@app.route('/')
-def main_route():
-    token = request.headers["Authorization"].split(" ")[1]
-    payload = jwt.decode(token, key, algorithms=['HS256'])
+def verify_token(token):
+    try:
+        payload = jwt.decode(token, key, algorithms=['HS256'])
+    except jwt.ExpiredSignatureError:
+        return False
     print("Token payload", payload)
     date = datetime.fromtimestamp(payload["exp"])
     print("Token expiration date", date)
-    return {"response": "ok"}
+    return True
+
+
+@app.route('/')
+@cross_origin()
+def main_route():
+    token = request.headers["Authorization"].split(" ")[1]
+    is_valid = verify_token(token)
+    if (is_valid is True):
+        return {"response": "ok"}
+    else:
+        abort(401)
